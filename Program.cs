0b5c3e758e14383a264d8ef450b89363c1027b1e
Lautaro Carro <lcarro@algeiba.com>
2024-06-23 02:51:59 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.SemanticKernel.ChatCompletion;
+
+using SemanticKernel.Api;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Nuestro metodo de extensión para realizar configuraciones de SemanticKernel
+builder.Services.AddSemanticKernel();
+
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/chat", async (ChatMessage request, IChatCompletionService chatService) =>
+{
+
+    ChatHistory chatMessages = new ChatHistory();
+
+    chatMessages.AddUserMessage(request.Message);
+
+    var chatResponse = await chatService.GetChatMessageContentAsync(chatMessages);
+
+    return Results.Ok(new ChatMessage(chatResponse.ToString()));
+})
+.WithName("Chat")
+.WithOpenApi();
+
+app.Run();
+
+record ChatMessage(string Message);
